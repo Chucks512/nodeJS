0b9ca26340c0c12b412ccf80cf5b916778aeda4d
chucks512 <thapelo.machaka14@gmail.com>
2024-07-01 16:15:35 +0200

    first upload

---
+++ listing 3.1/hello.js
@@ -0,0 +1,3 @@
+const messageModule = require("./newFile");
+messageModule.messages.forEach(m => console.log(m));
+console.log( messageModule.addNum(2,4) );
---
+++ listing 3.1/newFile.js
@@ -0,0 +1,10 @@
+"use strict";
+let messages = [
+"A change of environment can be a good thing!",
+"You will make it!",
+"Just run with the code!"
+];
+exports.messages = messages;
+exports.addNum = (x, y) => {
+    return x + y;
+    };
