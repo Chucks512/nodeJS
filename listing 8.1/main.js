982792e6a019358e187a42519d4a97195926917d
chucks512 <thapelo.machaka14@gmail.com>
2024-07-24 16:58:14 +0200

    listing 9.5 complete

--- listing 8.1/main.js
+++ listing 8.1/main.js
@@ -1,19 +1,33 @@
 "use strict";
 
 const port = 3000,
-  express = require("express"),
-  app = express();
+    express = require("express"),
+    app = express();
 
 app.use((req, res, next) => {
-  console.log(`request made to: ${req.url}`);
-  next();
+    console.log(`request made to: ${req.url}`);
+    next();
 });
 
+/*
 app.get("/items/:vegetable", (req, res) => {
   let veg = req.params.vegetable;
   res.send(`This is the page for ${veg}`);
+});*/
+
+
+app.use(
+    express.urlencoded({
+        extended: false
+    })
+);
+app.use(express.json());
+app.post("/", (req, res) => {
+    console.log(req.body);
+    console.log(req.query);
+    res.send("POST Successful!");
 });
 
 app.listen(port, () => {
-  console.log(`Server running on port: ${port}`);
+    console.log(`Server running on port: ${port}`);
 });
