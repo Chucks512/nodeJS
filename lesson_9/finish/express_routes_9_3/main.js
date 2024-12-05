75954533599f3802c7069c00620eb947403053e1
chucks512 <thapelo.machaka14@gmail.com>
2024-12-05 17:48:16 +0200

    lesson 9

---
+++ lesson_9/finish/express_routes_9_3/main.js
@@ -0,0 +1,30 @@
+"use strict";
+
+const port = 3000,
+  express = require("express"),
+  app = express(),
+  homeController = require("./controllers/homeController");//import controller
+
+app.use(
+  express.urlencoded({
+    extended: false//no extra data allowed in the URL link
+  })
+);
+app.use(express.json());
+
+app.use((req, res, next) => {
+  console.log(`request made to: ${req.url}`);//log URL
+  next();
+});
+
+app.post("/", (req, res) => {
+  console.log(req.body);
+  console.log(req.query);
+  res.send("POST Successful!");//res if successful
+});
+
+app.get("/items/:vegetable", homeController.sendReqParam);//get data or replave vegetable with other test word
+
+app.listen(port, () => {
+  console.log(`Server running on port: ${port}`);//log port
+});
